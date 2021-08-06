d788a036e3ca8796b92889656b2869716b09feee
Sarang <sarang@tocca.io>
2021-08-06 20:12:33 +0530

    change bucket path

--- src/recordingLambda/recording.js
+++ src/recordingLambda/recording.js
@@ -1,4 +1,4 @@
-const {"v4": uuidv4} = require('uuid');
+const { "v4": uuidv4 } = require('uuid');
 var AWS = require('aws-sdk');
 const chime = new AWS.Chime({ region: 'us-east-1' });
 chime.endpoint = new AWS.Endpoint('https://service.chime.aws.amazon.com/console');
@@ -8,34 +8,37 @@ const mediaCaptureBucket = process.env['MEDIA_CAPTURE_BUCKET']
 const aws_account_id = process.env['ACCOUNT_ID']
 
 exports.handler = async (event, context) => {
-    const body =  JSON.parse(event.body)
-    console.log(body) 
+    const body = JSON.parse(event.body)
+    console.log(body)
     const setRecording = body.setRecording
-    
+
     if (setRecording) {
         const deleteRequest = {
             "MediaPipelineId": body.mediaPipeLine
         }
         const deleteInfo = await chime.deleteMediaCapturePipeline(deleteRequest).promise()
         console.log(deleteInfo)
-                    const response = {
-                statusCode: 200,
-                body: JSON.stringify(deleteInfo),
-                headers: {
-                    'Access-Control-Allow-Origin':'*',
-                    'Access-Control-Allow-Headers':'*',
-                    'Access-Control-Allow-Methods': 'POST, OPTIONS',
-                    'Content-Type':'application/json'
-                }
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify(deleteInfo),
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Headers': '*',
+                'Access-Control-Allow-Methods': 'POST, OPTIONS',
+                'Content-Type': 'application/json'
             }
-            return response
+        }
+        return response
     } else {
         const meetingId = body.meetingId
+        const organizationId = body.organizationId
+        const eventId = body.eventId
+        const roomId = body.roomId
         const captureRequest = {
             "SourceType": "ChimeSdkMeeting",
             "SourceArn": "arn:aws:chime::" + aws_account_id + ":meeting:" + meetingId,
             "SinkType": "S3Bucket",
-            "SinkArn": "arn:aws:s3:::" + mediaCaptureBucket + "/captures/" + meetingId    
+            "SinkArn": "arn:aws:s3:::" + mediaCaptureBucket + "/captures/" + organizationId + "/" + eventId + "/" + roomId + "/" + meetingId,
         }
         try {
             const captureInfo = await chime.createMediaCapturePipeline(captureRequest).promise()
@@ -44,16 +47,16 @@ exports.handler = async (event, context) => {
                 statusCode: 200,
                 body: JSON.stringify(captureInfo),
                 headers: {
-                    'Access-Control-Allow-Origin':'*',
-                    'Access-Control-Allow-Headers':'*',
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Headers': '*',
                     'Access-Control-Allow-Methods': 'POST, OPTIONS',
-                    'Content-Type':'application/json'
+                    'Content-Type': 'application/json'
                 }
             }
             return response
         } catch (err) {
-           console.log(err)
-           return err
+            console.log(err)
+            return err
         }
     }
 }
